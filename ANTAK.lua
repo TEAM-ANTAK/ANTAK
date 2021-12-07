c6a5beacee2be6e5d46fdd784a4dcde010395c9c
TEAM-ANTAK <95723323+TEAM-ANTAK@users.noreply.github.com>
2021-12-08 02:46:39 +0300

    Add files via upload

--- ANTAK.lua
+++ ANTAK.lua
@@ -10769,7 +10769,7 @@ if SecondSudo(msg) then
 if text == "تحديث السورس" or text == "تحديث سورس" then 
 Dev_Abs(msg.chat_id_, msg.id_, 1, '⌔︙جاري تحديث سورس انتاك', 1, 'md') 
 os.execute('rm -rf venom.lua') 
-os.execute('wget https://raw.githubusercontent.com/Ven-Team/venom/master/venom.lua') 
+os.execute('wget https://raw.githubusercontent.com/TEAM-ANTAK/venom/master/venom.lua') 
 dofile('venom.lua') 
 io.popen("rm -rf ../.telegram-cli/*")
 print("\27[31;47m\n          ( تم تحديث السورس )          \n\27[0;34;49m\n") 
@@ -10797,7 +10797,7 @@ end
 send(msg.chat_id_, msg.id_,Files)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/Ven-Team/venomFiles/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/TEAM-ANTAK/venomFiles/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -10829,7 +10829,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/Ven-Team/venomFiles/master/LaricAFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/TEAM-ANTAK/venomFiles/master/LaricAFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n⌔︙الملف -› *"..FileName.."*\n⌔︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -10841,7 +10841,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/Ven-Team/venomFiles/master/LaricAFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/TEAM-ANTAK/ANTAKFiles/master/LaricAFiles/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
